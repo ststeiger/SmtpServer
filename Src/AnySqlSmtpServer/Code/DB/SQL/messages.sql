cb6ab8595c3cff378fe1b9a626d723cd27aa6503
Me <Me@example.com>
2022-06-19 15:24:47 +0200

    Table create statememtns

---
+++ Src/AnySqlSmtpServer/Code/DB/SQL/messages.sql
@@ -0,0 +1,24 @@
+﻿-- Table: public.messages
+
+-- DROP TABLE IF EXISTS public.messages;
+
+CREATE TABLE IF NOT EXISTS public.messages
+(
+     msg_uid uuid NOT NULL 
+    ,msg_mailbox character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_host character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_utc_timestamp timestamp without time zone 
+    ,msg_from_mailbox character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_from_host character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_unmapped character varying(255) COLLATE pg_catalog."default" 
+    ,msg_ip_v4 character varying(255) COLLATE pg_catalog."default" 
+    ,msg_ip_v6 character varying(255) COLLATE pg_catalog."default" 
+    ,msg_addressfamily character varying(255) COLLATE pg_catalog."default" 
+    ,msg_port integer 
+    ,msg_id character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_subject character varying(1000) COLLATE pg_catalog."default" 
+    ,msg_bytes bytea 
+    ,CONSTRAINT pk_messages PRIMARY KEY (msg_uid) 
+) TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.messages OWNER to smtp_verver_web_services;
---
+++ Src/AnySqlSmtpServer/Code/DB/SQL/messages_map_content.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public.messages_map_content
+
+-- DROP TABLE IF EXISTS public.messages_map_content;
+
+CREATE TABLE IF NOT EXISTS public.messages_map_content
+(
+     msg_uid uuid NOT NULL 
+    ,msg_bytes bytea 
+    ,msg_body character varying COLLATE pg_catalog."default" 
+    ,CONSTRAINT pk_messages_map_content PRIMARY KEY (msg_uid) 
+    ,CONSTRAINT fk_messages_map_content_messages FOREIGN KEY (msg_uid) 
+        REFERENCES public.messages (msg_uid) MATCH SIMPLE 
+        ON UPDATE NO ACTION 
+        ON DELETE NO ACTION 
+) TABLESPACE pg_default; 
+
+ALTER TABLE IF EXISTS public.messages_map_content OWNER to smtp_verver_web_services; 
